2c5db0aef75d78fb60295edd9870301d6dcc62ee
Kauan494 <kauan.elias.ribeiro@gmail.com>
2025-03-31 18:13:24 -0300

    projeto

--- Projetos/adivinhação.js
+++ Projetos/adivinhação.js
@@ -3,10 +3,10 @@ function number(){
     let r = Math.random()*100
     let i = Math.floor(r)
     console.log(i)
-    if (num >= i){
+    if (num === i){
         document.getElementById('h2_1').style.setProperty("background-color","green")
     }
-    else if(num <= i){
+    else if(num < i || num > i){
         document.getElementById('h2_1').style.setProperty("background-color","red")
     }
        
