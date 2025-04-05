d05c6fa3c883003e5fe029121eb3a6bbbe303729
Kauan494 <kauan.elias.ribeiro@gmail.com>
2025-04-05 11:35:45 -0300

    Update adivinhação.js

--- Projetos/adivinhação.js
+++ Projetos/adivinhação.js
@@ -1,8 +1,5 @@
 function number(){
     let num = parseInt(document.getElementById("number1").value)
-    let r = Math.random()*100
-    let i = Math.floor(r)
-    console.log(i)
     if (num === i){
         document.getElementById('h2_1').style.setProperty("background-color","green")
     }
