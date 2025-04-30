b3150745ad918f74d5fe1fe8a9ec48601901105f
Kauan494 <kauan.elias.ribeiro@gmail.com>
2025-04-29 22:43:40 -0300

    .

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -2,6 +2,15 @@ require('colors')
 var http = require('http');
 var express = require('express');
 let bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+const uri = 'mongodb+srv://kauan:O2WZn5IaWo2Rj2vS@cluster0.tzvmrin.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+var dbo = client.db("AulaBD");
+var usuarios = dbo.collection("usuarios");
+
 
 var app = express();
 app.use(express.static('./public'));
@@ -9,6 +18,7 @@ app.use(bodyParser.urlencoded({extended: false }));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs')
 app.set('views', './views');
+
 
 
 var server = http.createServer(app);
@@ -41,11 +51,42 @@ app.post("/cadastrar", function(requisicao, resposta) {
 
     console.log(nome, login, senha, nasc);
 
-    // Passando os dados como um objeto
-    resposta.render("resposta", {nome,login,senha,nasc});
+    var data = { db_nome: nome, db_login: login, db_senha: senha, db_nasc: nasc };
+    usuarios.insertOne(data, function(err){
+        console.log(err)
+        if(err){
+            resposta.render("resposta", {status: "Erro",nome,login,senha,nasc});
+        }else{
+            resposta.render("resposta", {status: "Sucesso",nome,login,senha,nasc})
+        }
+    })
+
+
+
+
+    
 });
 
 app.get("/for_ejs", function(requisicao,resposta){
     let valor = requisicao.query.valor;
     resposta.render("exemplo_for",{valor});
+})
+
+app.post('/logar', function(requisicao,resposta){
+    let login = requisicao.body.login
+    let senha = requisicao.body.senha
+    console.log(login,senha)
+    
+    var data = {db_login: login, db_senha: senha}
+    
+    usuarios.find(data).toArray(function(err,items){
+        if(items.length == 0){
+            resposta.render("resposta_login",{status: "usuario/senha não encontrados"});
+        }else if(err){
+            resposta.render("resposta_login",{status: "erro ao logar"});
+        }else{
+            resposta.render("resposta_login",{status: "usuario" +login+ "logado"})
+        }
+    })
+
 })
